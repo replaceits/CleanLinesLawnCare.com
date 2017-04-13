e37ba5b4ee64177adb70e3cb471c15eb9980793d
replaceits <sharpcut2231@gmail.com>
2017-04-13 02:40:54 -0400

    implement error messages

---
+++ errors/403.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" style="overflow: hidden;">
+    <head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>403 Forbidden - Clean Lines Lawn Care</title>
+
+		<base href="https://www.cleanlineslawncare.com/">
+
+		<link rel="stylesheet" href="css/CleanLinesLawnCare.com.css">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="/manifest.json">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#169113">
+        <meta name="theme-color" content="#ffffff">
+
+        <script defer type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script defer type="text/javascript" src="js/bootstrap.min.js"></script>
+	</head>
+
+	<body>
+		<main class="container-fluid text-center">
+            <div class="row">
+                <header class="col-xs-12">
+					<h1>403 - Forbidden!</h1>
+                </header>
+                <section>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <h4>We're sorry but you aren't allowed to do that.</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <a href="/">Click here to go back</a>
+                        </div>
+                    </div>
+                </section>
+			</div>
+		</main>
+
+        <footer class="container-fluid footer">
+            <div class="footer-item footer-item-left">
+                © 2017 Clean Lines Lawn Care
+            </div>
+            <div class="footer-item footer-item-right">
+                Site by <a href="https://www.replaceits.me">Sidney Williams</a>
+            </div>
+        </footer>
+	</body>
+</html>
